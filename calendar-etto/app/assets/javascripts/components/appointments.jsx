7e4371d61c372e6d22ce55d05eade1546e9101eb
Andi Mancuso <andi@andimancuso.com>
2017-09-08 20:36:49 -0500

    Implementing State

--- calendar-etto/app/assets/javascripts/components/appointments.jsx
+++ calendar-etto/app/assets/javascripts/components/appointments.jsx
@@ -1,7 +1,24 @@
 var Appointments = React.createClass({
+    getInitialState: function() {
+        return {
+            appointments: this.props.appointments,
+            title: 'Team standup meeting',
+            appt_time: '25 January 2018 9am'
+        }
+    },
+
+    handleUserInput: function(obj) {
+        this.setState(obj);
+    },
+
     render: function() {
         return (
-            <h1>CalReact</h1>
+            <div>
+                <AppointmentForm input_title={this.state.title}
+                                 input_appt_time={this.state.appt_time}
+                                 onUserInput={this.handleUserInput} />
+                <AppointmentsList appointments={this.state.appointments} />
+            </div>
         )
     }
-})
+});
